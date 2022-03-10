138b8e37464dbdd9e14fbc900517c90006cb1d72
Naveen <naveenp@drivevariant.com>
2022-03-10 12:39:48 -0500

    pre-commit fix

--- examples/main.tf
+++ examples/main.tf
@@ -2,11 +2,10 @@ data "aws_kms_key" "sns_alias" {
   key_id = "alias/ops/sns"
 }
 module "sqs_queue" {
-  source = "../"
-
+  source                   = "../"
   name                     = var.name
   aws_resource_name_prefix = var.aws_resource_name_prefix
-  topic_name           = var.topic_name
+  topic_name               = var.topic_name
   kms_key_sns_alias_arn    = data.aws_kms_key.sns_alias.arn
   tags                     = var.tags
 }
