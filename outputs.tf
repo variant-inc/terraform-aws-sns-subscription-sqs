3b10300187fde87d1768bd6968888a4fdc7d19dd
Samir Alyateem <31292258+salyateem13@users.noreply.github.com>
2022-05-09 09:27:18 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,13 +9,18 @@ output "sqs_queue_arn" {
 }
 
 output "sqs_queue_dlq_arn" {
-  description = "The ARN of the SQS DLQ queue"
+  description = "The ARN of the SQS DLQ"
   value       = module.sqs_queue_dlq.sqs_queue_arn
 }
 
 output "sqs_queue_name" {
-  description = "The ARN of the SQS queue"
+  description = "The name of the SQS queue"
   value       = module.sqs_queue.sqs_queue_name
+}
+
+output "sqs_queue_dlq_name" {
+  description = "The name of the SQS DLQ"
+  value       = module.sqs_queue_dlq.sqs_queue_name
 }
 
 output "queue_receive_policy" {
